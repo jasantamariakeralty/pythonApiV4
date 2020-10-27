0511360f21caedbd68a206eba44c5bc0cd27847b
jasantamariakeralty <64815790+jasantamariakeralty@users.noreply.github.com>
2020-10-27 08:15:58 -0500

    Update client.py

--- service/client.py
+++ service/client.py
@@ -7,11 +7,15 @@ class Client():
         self.last_name = last_name
         self.doc_id = doc_id
         self.preexistence = []
-
+    
+    """Adds a prexitence to a client
+    """
     def add_preexistence(self, n_preexistence):
         self.preexistence.append(n_preexistence)
         return len(self.preexistence) - 1
 
+    """Adds a prexitence to a client
+    """
     def get_preexistence(self, n_index):
         if n_index >= len(self.preexistence):
             return 'There is no such preexistence'
